06eb368ed8dbd1dcb029c19a558edf1f8896c027
Sujoy Das <sujoy@akijfood.com>
2024-01-14 21:58:16 +0600

    appointment POST

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.bn4iz8z.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctor_portal')
+        const appointmentsCollection = database.collection('appointments')
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+        })
+
+
+        console.log("Successfully connected to Atlas");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctor Portal')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/user/:id')
+// app.put('/user/:id')
+// app.delete('/user/:id')
